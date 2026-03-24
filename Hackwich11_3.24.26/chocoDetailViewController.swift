baabf502355ff5a77ed515dd2bb3f29eccb104dd
marionatuhwo <ano@hawaii.edu>
2026-03-24 13:51:39 -1000

    Completed TableView to Detail View Push for first item on tableview

---
+++ Hackwich11_3.24.26/chocoDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  chocoDetailViewController.swift
+//  Hackwich11_3.24.26
+//
+//  Created by Marion Ano on 3/24/26.
+//
+
+import UIKit
+
+class chocoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
