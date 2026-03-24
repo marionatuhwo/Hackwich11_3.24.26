1d4113b4d6a47fef34ac12c8e68329aec3112bf3
marionatuhwo <ano@hawaii.edu>
2026-03-24 11:47:30 -1000

    Completed tableview to display chocolate names

---
+++ Hackwich11_3.24.26/chocoTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  chocoTabViewController.swift
+//  Hackwich11_3.24.26
+//
+//  Created by Marion Ano on 3/24/26.
+//
+
+import UIKit
+
+class chocoTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
